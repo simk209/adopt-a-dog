a7152cd2b49556de4308eea210ee8cad5bfccb19
Simran Kaur <simran00kk@gmail.com>
2024-06-13 11:10:06 -0400

    prettier

--- src/components/ZipcodeFilter.tsx
+++ src/components/ZipcodeFilter.tsx
@@ -1,41 +1,43 @@
 interface ZipcodeFilterProps {
-    zipcodeInput: string;
-    setZipcodeInput: React.Dispatch<React.SetStateAction<string>>;
-    handleZipcodeKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-    zipcodeFilter: number[];
-  }
+  zipcodeInput: string;
+  setZipcodeInput: React.Dispatch<React.SetStateAction<string>>;
+  handleZipcodeKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  zipcodeFilter: number[];
+}
 
-const ZipcodeFilter = ({zipcodeFilter,handleZipcodeKeyDown,setZipcodeInput,zipcodeInput}:ZipcodeFilterProps) =>{
-
-    return (
-        <div className="mb-4">
-        <label htmlFor="zipcodeInput" className="block font-semibold mb-2">Filter by Zip Code:</label>
-          <input
-            type="text"
+const ZipcodeFilter = ({
+  zipcodeFilter,
+  handleZipcodeKeyDown,
+  setZipcodeInput,
+  zipcodeInput,
+}: ZipcodeFilterProps) => {
+  return (
+    <div className="mb-4">
+      <label htmlFor="zipcodeInput" className="block font-semibold mb-2">
+        Filter by Zip Code:
+      </label>
+      <input
+        type="text"
         id="zipcodeInput"
-    value={zipcodeInput}
-onChange={(e) => setZipcodeInput(e.target.value)}
-onKeyDown={handleZipcodeKeyDown}
-className="border border-gray-300 rounded-md px-2 py-1 w-full"
-placeholder="Enter zip code and press Enter"
-/>
-          {/* display applied zipcodes */}
-          <div className="mt-2">
-            {zipcodeFilter.map((zip, index) => (
-                <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                {zip}
-              </span>
-            ))}
-          </div>
-        </div>
+        value={zipcodeInput}
+        onChange={(e) => setZipcodeInput(e.target.value)}
+        onKeyDown={handleZipcodeKeyDown}
+        className="border border-gray-300 rounded-md px-2 py-1 w-full"
+        placeholder="Enter zip code and press Enter"
+      />
+      {/* display applied zipcodes */}
+      <div className="mt-2">
+        {zipcodeFilter.map((zip, index) => (
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >
+            {zip}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    )
-
-    
-        
-    
-    
-    }
-
-
-export default ZipcodeFilter
+export default ZipcodeFilter;
